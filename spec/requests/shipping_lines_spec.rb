f846aa141b5a6a17aad20cf228f285462bb47f56
chucoyos <unodev@outlook.com>
2025-12-01 15:14:18 -0600

    Fix shipping lines request specs to use correct paths

--- spec/requests/shipping_lines_spec.rb
+++ spec/requests/shipping_lines_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "ShippingLines", type: :request do
-  describe "GET /index" do
+  describe "GET /shipping_lines" do
     it "returns http success" do
-      get "/shipping_lines/index"
+      get shipping_lines_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
+  describe "GET /shipping_lines/:id" do
+    let!(:shipping_line) { ShippingLine.create!(name: "Test Line") }
+
     it "returns http success" do
-      get "/shipping_lines/show"
+      get shipping_line_path(shipping_line)
       expect(response).to have_http_status(:success)
     end
   end
