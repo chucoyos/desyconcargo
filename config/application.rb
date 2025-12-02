2bfbc7fda2bb8500e2a068953123a0f58f1462bc
chucoyos <unodev@outlook.com>
2025-12-01 23:09:56 -0600

    Set default locale to Spanish

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module Desyconcargo
     # Set timezone to Mexico City
     config.time_zone = 'America/Mexico_City'
 
+    # Set default locale to Spanish
+    config.i18n.default_locale = :es
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
