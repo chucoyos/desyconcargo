c668da452cfaa3ef60b7922da4aeb2ef6dcc9fc7
chucoyos <unodev@outlook.com>
2025-12-02 03:47:26 -0600

    Fix empty fields validation

--- app/models/vessel.rb
+++ app/models/vessel.rb
@@ -2,4 +2,5 @@ class Vessel < ApplicationRecord
   belongs_to :shipping_line
 
   validates :name, presence: true, uniqueness: { scope: :shipping_line_id }
+  validates :shipping_line_id, presence: true
 end
