57c79016dd12613edcf7f901df0d2b4a04d4e731
chucoyos <unodev@outlook.com>
2025-12-03 17:21:22 -0600

    Fix Rubocop offences

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -87,5 +87,5 @@ Rails.application.configure do
   end
 
   # Devise requires default url options
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 end
