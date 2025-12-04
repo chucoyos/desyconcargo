405297ac4a7b4d19441712a459b5f930af5823b7
chucoyos <unodev@outlook.com>
2025-12-04 10:33:39 -0600

    Fix role creation error

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -73,6 +73,6 @@ class RolesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def role_params
-      params.fetch(:role, {})
+      params.require(:role).permit(:name)
     end
 end
